284029a1b172ec90093ecce011b601eb745bf410
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2019-07-22 12:07:39 +0200

    Added how to sort code, added main

--- src/main/main.c
+++ src/main/main.c
@@ -12,7 +12,44 @@
 
 #include "../ft_ls.h"
 
+void	init_args(t_args *args)
+{
+	(*args).long_list = 0;
+	(*args).all = 0;
+	(*args).noowner = 0;
+	(*args).no_recurse = 0;
+	(*args).no_sort = 0;
+	(*args).reverse_sort = 0;
+	(*args).sort_time = 0;
+	(*args).sort_access = 0;
+	(*args).colour = 0;
+}
+
 int		main(int argc, char **argv)
 {
-	
+	t_args	args;
+	int		i;
+	char	got_path;
+
+	init_args(&args);
+	i = 1;
+	got_path = 0;
+	while (i < argc)
+	{
+		if (ft_strncmp(argv[i], "-", 1) == 0)
+			parse_args(&args, argv[i]);
+		else if (args.recursive)
+			ft_putstr("Not implemented\n");
+		else
+			show_folder(argv[i], args);
+		i++;
+	}
+	if (!got_path)
+	{
+		if (args.recursive)
+			ft_putstr("Not implemented\n");
+		else
+			show_folder(".", args);
+	}
+	return (0);
 }
--- src/main/sort.c
+++ src/main/sort.c
@@ -12,7 +12,32 @@
 
 #include "../ft_ls.h"
 
-void	sort_elem_array(t_elem *arr, t_args args)
+void	sort_elems_name(t_elem *arr)
 {
 	
+}
+
+void	sort_elems_modified_time(t_elem *arr, char reverse)
+{
+	
+}
+
+void	sort_elems_access_time(t_elem *arr, char reverse)
+{
+	
+}
+
+void	sort_elem_array(t_elem *arr, t_args args)
+{
+	if (args.no_sort)
+		return ;
+	else if (args.sort_time)
+	{
+		if (args.sort_access)
+			sort_elems_access_time(arr, args.reverse_sort);
+		else
+			sort_elems_modified_time(arr, args.reverse_sort);
+	}
+	else
+		sort_elems_name(arr);
 }
