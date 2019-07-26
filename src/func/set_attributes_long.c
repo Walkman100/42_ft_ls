8f3b168e8ead63d3231feef239b3a6f37c325bea
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2019-07-26 17:32:40 +0200

    unified linux & mac set_attributes_*

--- src/func/set_attributes_long.c
+++ src/func/set_attributes_long.c
@@ -6,13 +6,11 @@
 /*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/26 11:19:57 by mcarter           #+#    #+#             */
-/*   Updated: 2019/07/26 16:53:08 by mcarter          ###   ########.fr       */
+/*   Updated: 2019/07/26 17:31:58 by mcarter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_ls.h"
-
-#ifdef __linux__
 
 void	set_attributes_long(t_elem *item)
 {
@@ -30,24 +28,3 @@ void	set_attributes_long(t_elem *item)
 	if ((*item).type == 'l')
 		(*item).ln_target = get_slink_target((*item).name, stat_s.st_size);
 }
-
-#else
-
-void	set_attributes_long(t_elem *item)
-{
-	struct stat	stat_s;
-
-	lstat((*item).name, &stat_s);
-	(*item).atime = stat_s.st_atimespec.tv_sec;
-	(*item).mtime = stat_s.st_mtimespec.tv_sec;
-	parse_mode(stat_s.st_mode, &(*item).type, &(*item).perms);
-	(*item).inodes = stat_s.st_nlink;
-	(*item).user = parse_user(stat_s.st_uid);
-	(*item).group = parse_group(stat_s.st_gid);
-	(*item).fsize = stat_s.st_size;
-	(*item).date = get_last_modified(stat_s.st_mtimespec.tv_sec);
-	if ((*item).type == 'l')
-		(*item).ln_target = get_slink_target((*item).name, stat_s.st_size);
-}
-
-#endif
--- src/func/set_attributes_short.c
+++ src/func/set_attributes_short.c
@@ -6,13 +6,11 @@
 /*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/26 11:20:13 by mcarter           #+#    #+#             */
-/*   Updated: 2019/07/26 16:53:47 by mcarter          ###   ########.fr       */
+/*   Updated: 2019/07/26 17:32:04 by mcarter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_ls.h"
-
-#ifdef __linux__
 
 void	set_attributes_short(t_elem *item)
 {
@@ -23,17 +21,3 @@ void	set_attributes_short(t_elem *item)
 	(*item).mtime = stat_s.st_mtime;
 	(*item).type = get_type(&stat_s.st_mode);
 }
-
-#else
-
-void	set_attributes_short(t_elem *item)
-{
-	struct stat	stat_s;
-
-	lstat((*item).name, &stat_s);
-	(*item).atime = stat_s.st_atimespec.tv_sec;
-	(*item).mtime = stat_s.st_mtimespec.tv_sec;
-	(*item).type = get_type(&stat_s.st_mode);
-}
-
-#endif
