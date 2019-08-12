1cadb0aa247248ed596ae11b600a3d7fc1916c38
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2019-08-12 15:37:24 +0200

    fixed reverse sort

--- src/main/sort.c
+++ src/main/sort.c
@@ -6,7 +6,7 @@
 /*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/18 14:05:13 by mcarter           #+#    #+#             */
-/*   Updated: 2019/07/29 15:31:35 by mcarter          ###   ########.fr       */
+/*   Updated: 2019/08/12 15:35:20 by mcarter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,28 +37,20 @@ void	sort_elems(t_elem *arr, int (*cmp)(t_elem, t_elem))
 	}
 }
 
-void	sort_elems_reverse(t_elem *arr, int (*cmp)(t_elem, t_elem))
+void	reverse_array(t_elem *arr)
 {
-	int		i;
-	int		b_sorted;
+	size_t	i;
+	size_t	arr_len;
 	t_elem	temp;
 
-	b_sorted = 0;
-	while (b_sorted == 0)
+	i = 0;
+	arr_len = get_elem_count(arr);
+	while (i < arr_len / 2)
 	{
-		b_sorted = 1;
-		i = 0;
-		while (arr[i].name && arr[i + 1].name)
-		{
-			if (cmp(arr[i], arr[i + 1]) < 0)
-			{
-				temp = arr[i];
-				arr[i] = arr[i + 1];
-				arr[i + 1] = temp;
-				b_sorted = 0;
-			}
-			i++;
-		}
+		temp = arr[i];
+		arr[i] = arr[arr_len - i - 1];
+		arr[arr_len - i - 1] = temp;
+		i++;
 	}
 }
 
@@ -77,8 +69,7 @@ void	sort_elem_array(t_elem *arr, t_args args)
 	}
 	else
 		cmp = cmp_name;
-	if (args.reverse_sort == 0)
-		sort_elems(arr, cmp);
-	else
-		sort_elems_reverse(arr, cmp);
+	sort_elems(arr, cmp);
+	if (args.reverse_sort)
+		reverse_array(arr);
 }
