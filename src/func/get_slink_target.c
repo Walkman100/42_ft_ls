6e88a6b82421dbcf26eae36dee22f63b42cd0fb2
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2019-08-26 17:25:38 +0200

    fixed getting symlink target of symlink with reported 0 target

--- src/func/get_slink_target.c
+++ src/func/get_slink_target.c
@@ -6,7 +6,7 @@
 /*   By: mcarter <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/22 11:21:33 by mcarter           #+#    #+#             */
-/*   Updated: 2019/08/05 16:48:56 by mcarter          ###   ########.fr       */
+/*   Updated: 2019/08/26 17:13:24 by mcarter          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,15 @@ char	*get_slink_target(char *path, MAXUNBR targetlen)
 	char	*rtn;
 	int		len;
 
-	rtn = ft_memalloc(sizeof(*rtn) * (targetlen + 1));
-	if ((len = readlink(path, rtn, targetlen + 1)) != -1)
+	if (!targetlen)
+		targetlen = 128;
+	rtn = ft_strnew(targetlen);
+	if ((len = readlink(path, rtn, targetlen)) != -1)
 	{
 		rtn[len] = '\0';
 		return (rtn);
 	}
-	else
-	{
-		put_error(errno, "readlink ", __func__);
-		free(rtn);
-		return (ft_strjoin("Error: ", strerror(errno)));
-	}
+	put_error(errno, "readlink ", __func__);
+	free(rtn);
+	return (ft_strjoin("Error: ", strerror(errno)));
 }
